6da2349562fa5019a7f92b964c13afd7309d7755
akiresu15 <57805737+akiresu15@users.noreply.github.com>
2024-09-02 20:25:45 +0800

    Update

---
+++ infinite loop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    while (true) {  // This creates an infinite loop
+        std::cout << "Hello" << std::endl;
+    }
+
+    return 0;  // This line will never be reached
+}
